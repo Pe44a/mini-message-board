811d0e963b29890b17366e6be612d53ba56882a8
Peteris <blankfeldspeteris@gmail.com>
2024-06-25 14:54:27 +0300

    Renders messages at '/new' route

--- express-mini-message-board/routes/index.js
+++ express-mini-message-board/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/new', function(req, res, next) {
+  const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
+  res.render('index', { title: "Mini Messageboard", messages: messages })
+});
+
+
 module.exports = router;
